06a77d5bbcb0205add0d6b0c05c6e3757d0ea00b
kishore madugula <kishore.madugula@outlook.com>
2024-02-12 19:46:40 -0500

    working all except Acknowledgement

--- springboot-kafka-avro-producer/src/main/java/org/poc/kafka/mapper/StudentMapper.java
+++ springboot-kafka-avro-producer/src/main/java/org/poc/kafka/mapper/StudentMapper.java
@@ -7,6 +7,6 @@ import org.poc.kafka.avro.model.Student;
 @Mapper(componentModel = "spring")
 public interface StudentMapper {
 
-  @Mapping(target = "className", ignore = true)
+  @Mapping(target = "contactBuilder", ignore = true)
   Student mapStudentApiDataToStudentAvroData(org.poc.kafka.api.model.Student student);
 }
